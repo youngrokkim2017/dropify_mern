0fd338a0dc5c60fc81cc76975c173a03b5c9c64d
Patrick <patrick.yrkim@gmail.com>
2020-04-25 23:25:03 -0700

    Add current position to music map

--- frontend/src/components/music/music_map/music_map.jsx
+++ frontend/src/components/music/music_map/music_map.jsx
@@ -7,32 +7,43 @@ const mapStyles = {
 };
 
 class MusicMap extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "React",
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "React",
+        };
+    }
 
-  render() {
-    return (
-      <div>
-        <Map
-          google={this.props.google}
-          zoom={14}
-          style={mapStyles}
-          initialCenter={{
-            // lat: YOUR_LATITUDE,
-            lat: 36,
-            // lng: YOUR_LONGITUDE,
-            lng: -119,
-          }}
-        >
-          <Marker onClick={this.onMarkerClick} name={"This is test name"} />
-        </Map>
-      </div>
-    );
-  }
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                console.log(position)
+            },
+            function (error) {
+                    console.error("Error Code = " + error.code + " - " + error.message);
+            }
+        );
+    }
+
+    render() {
+        return (
+        <div>
+            <Map
+            google={this.props.google}
+            zoom={14}
+            style={mapStyles}
+            initialCenter={{
+                // lat: YOUR_LATITUDE,
+                lat: 36,
+                // lng: YOUR_LONGITUDE,
+                lng: -119,
+            }}
+            >
+            <Marker onClick={this.onMarkerClick} name={"This is test name"} />
+            </Map>
+        </div>
+        );
+    }
 }
 
 // export default GoogleApiWrapper({
