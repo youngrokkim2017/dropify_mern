f77fdf23749d46a31e80515129291ccd30dda28c
Patrick <patrick.yrkim@gmail.com>
2020-05-11 23:59:56 -0700

    Add sass

--- frontend/src/components/music/music_box.jsx
+++ frontend/src/components/music/music_box.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import '../../stylesheets/music_index_item.scss';
 
 class MusicBox extends React.Component {
     render() {
         return (
             <div>
-                <h3>{this.props.title}</h3>
+                {/* <h3>{this.props.title}</h3>
                 <h3>{this.props.artist}</h3>
                 <h3>{this.props.genre}</h3>
-                <h3>{this.props.location}</h3>
+                <h3>{this.props.location}</h3> */}
                 {/* <h3>{this.props.user}</h3> */}
+                <table>
+                    <thead></thead>
+                    <tr>
+                        <th>{this.props.title}</th>
+                        <th>{this.props.artist}</th>
+                        <th>{this.props.genre}</th>
+                        {/* <th>{this.props.location}</th> */}
+                    </tr>
+                </table>
             </div>
         );
     };
